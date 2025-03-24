9be529544ca6a3eaa19c6a3b0c8d0546ac400862
Loc Doan <u1438103@utah.edu>
2025-03-24 03:26:31 -0600

    add setup_hook to load cogs

--- weather_bot.py
+++ weather_bot.py
@@ -17,7 +17,10 @@ async def on_ready():
     print(f'Logged in as {bot.user}')
 
 # Load cogs
-bot.load_extension('cogs.weather')
-bot.load_extension('cogs.forecast')
+@bot.event
+async def setup_hook():
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cogs.{filename[:-3]}')
 
 bot.run(os.getenv('DISCORD_BOT_TOKEN'))
